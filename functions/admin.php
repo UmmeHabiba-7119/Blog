0bd437fcd6a1367005a5b2d89ad4a95b41258fa9
UmmeHabiba-7119 <habiba15-7119@diu.edu.bd>
2020-04-29 23:54:14 -0700

    Database Connection

---
+++ functions/admin.php
@@ -0,0 +1,19 @@
+<?php
+
+/*****************************************************************************************************
+this page maintain all the functions for Admin
+**********************************************************************/
+
+    //page joining class
+    require_once '../Config/config.php';
+    require_once '../Classes/pageContent.php';
+    require_once '../Classes/database.php';
+
+    //object of pageContent class
+    $page_content = new pageContent();
+    //object of Database class
+    $db = new Database();
+
+
+
+ ?>
